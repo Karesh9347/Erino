95de996e8aafa8399cbf6c3e8454f4a81d0a7cf0
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-11-16 04:17:08 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,10 +21,10 @@ const Home = () => {
           backgroundImage: `linear-gradient(45deg, ${theme.palette.primary.main}, #ffffff)`, 
         }}
       >
-        <Typography variant="h3" sx={{ fontWeight: 'bolder', fontFamily: 'sans-serif' }}>
+        <Typography variant="h3" sx={{ fontWeight: 'bolder', fontFamily:"times-new-roman",fontPalette:"dark" }}>
           Sell More. Pay Less. <br /> Grow Fast 🚀
         </Typography>
-        <Typography variant="h5" gutterBottom style={{ padding: '0 10%' }}>
+        <Typography variant="h5" gutterBottom style={{ padding: '0 10%',fontFamily:"serif" }}>
           Erino CRM: Sales, service, and journey building tools to 2X your growth.
           <br />
            Start in less than 5 minutes and see instant results.
@@ -35,8 +35,9 @@ const Home = () => {
             color: 'white',
             fontWeight: 'bold',
             backgroundColor: "ActiveBorder", 
-            borderRadius: '10px',
+            borderRadius: '20px',
             padding: '15px',
+            margin:"10px",
             fontSize: [theme.typography.fontSize.h6, theme.typography.fontSize.h4], 
             
           }}
